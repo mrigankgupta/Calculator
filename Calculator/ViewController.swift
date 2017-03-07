4e98b81af59dcc7f5877ee4314cfdbe6fb2200bd
Mrigank Gupta <mrigank.gupta@honeywell.com>
2017-03-07 20:31:42 +0530

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Gupta, Mrigank on 07/03/17.
+//  Copyright © 2017 Gupta, Mrigank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
